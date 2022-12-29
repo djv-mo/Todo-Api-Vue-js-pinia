ff1cf08c0104b2b3a5105a43c85dabb195948543
mohamed Ali <solutiond700@gmail.com>
2022-12-29 20:01:46 +0200

    initial setup

---
+++ todo/config/local.py
@@ -0,0 +1,35 @@
+import os
+from .common import Common
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+class Local(Common):
+    DEBUG = True
+
+    # Testing
+    INSTALLED_APPS = Common.INSTALLED_APPS
+    INSTALLED_APPS += ('django_nose', "debug_toolbar",
+                       )
+    MIDDLEWARE = Common.MIDDLEWARE
+    MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",
+
+                   )
+    TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+    NOSE_ARGS = [
+        BASE_DIR,
+        '-s',
+        '--nologcapture',
+        '--with-coverage',
+        '--with-progressive',
+        '--cover-package=todo'
+    ]
+
+    # Mail
+    EMAIL_HOST = 'localhost'
+    EMAIL_PORT = 1025
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    INTERNAL_IPS = [
+        # ...
+        "127.0.0.1",
+        # ...
+    ]
