81f10a9340d91eb81a289b137c9e76a80c3fe983
mohamed Ali <solutiond700@gmail.com>
2022-12-29 20:31:02 +0200

    bulding task model

--- todo/task/models.py
+++ todo/task/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    created = models.DateField(auto_now_add=True)
+    done = models.BooleanField(default=False)
+    fav = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
